a69416251afd18f2b8e0ac3a7d5ab1da86af4c03
shunliz <zsl6658@126.com>
2019-01-23 19:22:08 -0500

    add vm

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 cc = gcc
 prom = clox
-source = chunk.c debug.c main.c memory.c value.c
+source = chunk.c debug.c main.c memory.c value.c vm.c
 
 $(prom): $(source)
 	$(cc) -o $(prom) $(source)
+
+clean:
+	rm -rf clox
---
+++ vm.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: vm.h
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 23 Jan 2019 06:48:53 PM EST
+ ************************************************************************/
+#ifndef clox_vm_h 
+#define clox_vm_h 
+
+#include "chunk.h"
+
+typedef struct {  
+  Chunk* chunk;   
+  uint8_t* ip;
+} VM;             
+
+typedef enum {            
+  INTERPRET_OK,           
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR 
+} InterpretResult;
+
+void initVM();    
+void freeVM();    
+InterpretResult interpret(Chunk* chunk);
+
+#endif   
