f1877316dd07c515c07b435c04c460fb07409d50
shunliz <zsl6658@126.com>
2019-01-24 18:46:01 -0500

    add vm

--- vm.h
+++ vm.h
@@ -8,10 +8,15 @@
 #define clox_vm_h 
 
 #include "chunk.h"
+#include "value.h"  
+
+#define STACK_MAX 256
 
 typedef struct {  
   Chunk* chunk;   
   uint8_t* ip;
+  Value stack[STACK_MAX];
+  Value* stackTop;     
 } VM;             
 
 typedef enum {            
@@ -23,5 +28,7 @@ typedef enum {
 void initVM();    
 void freeVM();    
 InterpretResult interpret(Chunk* chunk);
+void push(Value value);                 
+Value pop();  
 
 #endif   
